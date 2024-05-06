ae5f90ed022025088adc49daa7249320904f2f30
Stanley Meruo <144041200+stanley-meruo@users.noreply.github.com>
2024-05-06 22:00:54 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("sideBar").style.width = "100%";
+  document.getElementById("sideNav").style.width = "100%";
+}
+
+/*Close navigation*/
+function exitNav() {
+  document.getElementById("sideBar").style.width = "0";
+  document.getElementById("sideNav").style.width = "0";
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "alata": ["Alata", "sans"],
+      "josefin-sans": ["Josefin Sans", "sans"],
+    },
+    extend: {
+      backgroundImage: {
+        "hero-m-bg": "url('/images/mobile/image-hero.jpg')",
+        "hero-d-bg": "url('/images/desktop/image-hero.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
+
